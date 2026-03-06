1bef5a56cda899de7b2baf7a8c8a6b16fbdc4229
Gilvan Pedro <87739497+GilvanPedro@users.noreply.github.com>
2026-03-05 21:55:50 -0300

    Add files via upload

---
+++ Pilha/src/main/java/br/com/No2.java
@@ -0,0 +1,27 @@
+package br.com;
+
+public class No2 {
+    private String guiaFechada;
+    private No2 proximo;
+
+    public No2(String guiaFechada) {
+        this.guiaFechada = guiaFechada;
+        this.proximo = null;
+    }
+
+    public String getGuiaFechada() {
+        return guiaFechada;
+    }
+
+    public void setGuiaFechada(String guiaFechada) {
+        this.guiaFechada = guiaFechada;
+    }
+
+    public No2 getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No2 proximo) {
+        this.proximo = proximo;
+    }
+}
---
+++ Pilha/src/main/java/br/com/Pilha.java
@@ -0,0 +1,92 @@
+package br.com;
+
+public class Pilha {
+    private No topo = null;
+    private No2 topo2 = null;
+
+    // Inserir no topo
+    public void push(String guia) {
+        No novo = new No(guia);
+        novo.setProximo(topo);
+        topo = novo;
+    }
+
+    // Exibir elementos
+    public void consultar() {
+        if (topo2 == null) {
+            System.out.println("Nenhuma guia aberta");
+            return;
+        }
+
+        No atual = topo;
+        while (atual != null) {
+            System.out.println(atual.getGuiaAberta());
+            atual = atual.getProximo();
+        }
+    }
+
+    public String retornarGuia(){
+        if (topo != null) {
+            return topo.getGuiaAberta();
+        }
+        return null;
+    }
+    // Remover topo
+    public String pop() {
+        if (topo == null) {
+            System.out.println("Não há guias para fechar");
+            return null;
+        }
+
+        String valor = topo.getGuiaAberta();
+        topo = topo.getProximo();
+
+        //adicionando o valor retirado na pila na outra pilha de guias excluidas
+        guiasFechadas(valor);
+
+        return valor;
+    }
+
+    // Adicionando uma nova pilha das guias fechadas
+    public void guiasFechadas(String valor) {
+        No2 novo = new No2(valor);
+        novo.setProximo(topo2);
+        topo2 = novo;
+    }
+
+    // Mostrando as guias que foram fechadas
+    public void exibirGuiasFechadas(){
+        if (topo2 == null) {
+            System.out.println("A pilha está vazia");
+            return;
+        }
+
+        No2 atual = topo2;
+        while (atual != null) {
+            System.out.println(atual.getGuiaFechada());
+            atual = atual.getProximo();
+        }
+    }
+
+    // Reabre as guias fechadas
+    public String reabrirGuiasFechadas(){
+        if (topo2 == null) {
+            System.out.println("Não há guias para reabrir");
+            return null;
+        }
+
+        String valor = topo2.getGuiaFechada();
+        topo2 = topo2.getProximo();
+        push(valor);
+
+        return valor;
+    }
+
+    // Retorna a última guia fechada
+    public String guiaretornada(){
+        if(topo2 != null) {
+            return topo2.getGuiaFechada();
+        }
+        return null;
+    }
+}
